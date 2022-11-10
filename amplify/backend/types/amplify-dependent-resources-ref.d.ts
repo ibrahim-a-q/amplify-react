a855fb11c25574d20b5002cf2359fe0d59730fed
ibrahim-aq <ibrahimabdulqadi.1@gmail.com>
2022-11-10 10:20:59 -0600

    added graphql and storage

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -9,5 +9,18 @@ export type AmplifyDependentResourcesAttributes = {
             "AppClientIDWeb": "string",
             "AppClientID": "string"
         }
+    },
+    "api": {
+        "amplifyreact": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "storage": {
+        "s3d85f05e4": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
